5d90a1edb839ce9752547d7f40d3ce8e984876b9
origin <acmuhammadmustafa@gmail.com>
2025-10-02 15:30:09 -0400

    finalized exercise

---
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int loveJava= 0;
+        while (loveJava < 5){
+            System.out.println("I love Java");
+            loveJava++;
+        }
+
+        }
+    }
+
+
